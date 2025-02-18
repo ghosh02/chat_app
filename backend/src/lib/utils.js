06d2fa26a10464001691d76284a86b3d60dbc5f4
Sudip Ghosh <sudip02ghosh@gmail.com>
2025-02-18 23:19:13 +0530

    change in api

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ module.exports.generateToken = (userId, res) => {
     maxAge: 1000 * 60 * 60 * 24 * 7,
     httpOnly: true,
     sameSite: "strict",
-    // secure: true,
+    secure: process.env.NODE_ENV !== "development",
   });
   return token;
 };
