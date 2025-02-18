9d190dcdcf55b543e023f4ba5f6bdcd86d4d84cd
Sudip Ghosh <sudip02ghosh@gmail.com>
2025-02-19 00:34:35 +0530

    error

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const path = require("path");
 
 const port = process.env.PORT || 3000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
@@ -22,13 +22,13 @@ app.use(
 );
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
